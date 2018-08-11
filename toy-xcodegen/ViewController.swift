d67925718f9362407c67d8dafbdfd5e8c46d1c57
Faiz Mokhtar <mfmokhtar@gmail.com>
2018-08-11 16:03:36 +0800

    initial commit

---
+++ toy-xcodegen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  toy-xcodegen
+//
+//  Created by Faiz Mokhtar on 11/08/2018.
+//  Copyright © 2018 faizmokhtar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
